21020e7f2abbe2a9803b09b8a0af78a36723ec7e
user <user@DESKTOP-0175HAA>
2020-09-03 17:32:46 +0900

    9월 3일 목요일. 전자정부 프로젝트 AdminLTE게시판 삭제 기능 마무리

--- src/main/java/edu/human/com/board/web/BoardController.java
+++ src/main/java/edu/human/com/board/web/BoardController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.inject.Inject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springmodules.validation.commons.DefaultBeanValidator;
 
+import edu.human.com.board.service.BoardService;
 import egovframework.com.cmm.EgovMessageSource;
 import egovframework.com.cmm.LoginVO;
 import egovframework.com.cmm.service.EgovFileMngService;
@@ -32,7 +34,6 @@ import egovframework.let.cop.bbs.service.EgovBBSManageService;
 import egovframework.rte.fdl.property.EgovPropertyService;
 import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 
-
 @Controller
 public class BoardController {
 
@@ -46,159 +47,190 @@ public class BoardController {
     private EgovBBSManageService bbsMngService;
    
    @Resource(name="egovMessageSource")
-   EgovMessageSource egovMessageSource;
+    EgovMessageSource egovMessageSource;
    
    @Autowired
-   private DefaultBeanValidator beanValidator;
+    private DefaultBeanValidator beanValidator;
    
    @Resource(name = "EgovFileMngService")
-   private EgovFileMngService fileMngService;
+    private EgovFileMngService fileMngService;
 
-   @Resource(name = "EgovFileMngUtil")
-   private EgovFileMngUtil fileUtil;
+    @Resource(name = "EgovFileMngUtil")
+    private EgovFileMngUtil fileUtil;
+    
+    @Inject
+    private BoardService boardService;
    
    /**
-    * XSS 방지 처리.
-    *
-    * @param data
-    * @return
-    */
-   protected String unscript(String data) {
-       if (data == null || data.trim().equals("")) {
-           return "";
-       }
+     * XSS 방지 처리.
+     *
+     * @param data
+     * @return
+     */
+    protected String unscript(String data) {
+        if (data == null || data.trim().equals("")) {
+            return "";
+        }
 
-       String ret = data;
+        String ret = data;
 
-       ret = ret.replaceAll("<(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;script");
-       ret = ret.replaceAll("</(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;/script");
+        ret = ret.replaceAll("<(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;script");
+        ret = ret.replaceAll("</(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;/script");
 
-       ret = ret.replaceAll("<(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;object");
-       ret = ret.replaceAll("</(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;/object");
+        ret = ret.replaceAll("<(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;object");
+        ret = ret.replaceAll("</(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;/object");
 
-       ret = ret.replaceAll("<(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;applet");
-       ret = ret.replaceAll("</(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;/applet");
+        ret = ret.replaceAll("<(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;applet");
+        ret = ret.replaceAll("</(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;/applet");
 
-       ret = ret.replaceAll("<(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
-       ret = ret.replaceAll("</(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
+        ret = ret.replaceAll("<(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
+        ret = ret.replaceAll("</(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
 
-       ret = ret.replaceAll("<(F|f)(O|o)(R|r)(M|m)", "&lt;form");
-       ret = ret.replaceAll("</(F|f)(O|o)(R|r)(M|m)", "&lt;form");
+        ret = ret.replaceAll("<(F|f)(O|o)(R|r)(M|m)", "&lt;form");
+        ret = ret.replaceAll("</(F|f)(O|o)(R|r)(M|m)", "&lt;form");
 
-       return ret;
-   }
+        return ret;
+    }
    
-   /*
-    * AdminLTE용 게시물 수정(단, 상세보기+수정 함께사용으로 개선).
+   
+    /*
+     * AdminLTE용 게시물 삭제(관리자삭제는 테이블 레코드-1줄 을 삭제처리)
+     * 기존에는 게시판 레코드-1줄에서 USE_AT필드값을 N으로 변경해서 처리 와 차이있음.
+     */
+    @RequestMapping("/admin/board/deleteBoard.do")
+    public String deleteBoard(
+          @ModelAttribute("searchVO") BoardVO boardVO,
+          @ModelAttribute("bdMstr") BoardMaster bdMstr,
+          @ModelAttribute("board") Board board,
+          ModelMap model
+          ) throws Exception {
+       //현재 기존 삭제방식 입니다.
+       LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+          board.setLastUpdusrId(user.getUniqId());
+          bbsMngService.deleteBoardArticle(board);
+          //여기까지 진행되면, 기존삭제 로직은 마무리, 그대로 유지(여기에 파일삭제 기능까지 있음)
+          //이후에 개발자가 완전삭제 코드를 추가하면 됩니다.(아래-레코드 삭제 목적)
+          boardService.deleteBoard(board.getNttId());
+
+       return "forward:/admin/board/selectBoard.do";
+    }
+    /*
+    * AdminLTE용 게시물 수정(관리자단, 상세보기+수정 함께사용으로 개선).
     */
    @RequestMapping("/admin/board/updateBoard.do")
-   public String updateBoard(final MultipartHttpServletRequest multiRequest, @ModelAttribute("searchVO") BoardVO boardVO,
-		    @ModelAttribute("bdMstr") BoardMaster bdMstr, @ModelAttribute("board") Board board, BindingResult bindingResult, ModelMap model,
-		    SessionStatus status)throws Exception {
-	// 사용자권한 처리
-   	if(!EgovUserDetailsHelper.isAuthenticated()) {
-   		model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
-       	return "cmm/uat/uia/EgovLoginUsr";
-   	}
+   public String updateBoard(final MultipartHttpServletRequest multiRequest,
+         @ModelAttribute("searchVO") BoardVO boardVO,
+          @ModelAttribute("bdMstr") BoardMaster bdMstr,
+          @ModelAttribute("board") Board board,
+          BindingResult bindingResult, ModelMap model,
+          SessionStatus status) throws Exception {
+      
+      // 사용자권한 처리
+       if(!EgovUserDetailsHelper.isAuthenticated()) {
+          model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
+           return "cmm/uat/uia/EgovLoginUsr";
+       }
 
-	LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
-	Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+   LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+   Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
 
-	String atchFileId = boardVO.getAtchFileId();
+   String atchFileId = boardVO.getAtchFileId();
 
-	beanValidator.validate(board, bindingResult);
-	if (bindingResult.hasErrors()) {
+   beanValidator.validate(board, bindingResult);
+   if (bindingResult.hasErrors()) {
 
-	    boardVO.setFrstRegisterId(user.getUniqId());
+       boardVO.setFrstRegisterId(user.getUniqId());
 
-	    BoardMaster master = new BoardMaster();
-	    BoardMasterVO bmvo = new BoardMasterVO();
-	    BoardVO bdvo = new BoardVO();
+       BoardMaster master = new BoardMaster();
+       BoardMasterVO bmvo = new BoardMasterVO();
+       BoardVO bdvo = new BoardVO();
 
-	    master.setBbsId(boardVO.getBbsId());
-	    master.setUniqId(user.getUniqId());
+       master.setBbsId(boardVO.getBbsId());
+       master.setUniqId(user.getUniqId());
 
-	    bmvo = bbsAttrbService.selectBBSMasterInf(master);
-	    bdvo = bbsMngService.selectBoardArticle(boardVO);
+       bmvo = bbsAttrbService.selectBBSMasterInf(master);
+       bdvo = bbsMngService.selectBoardArticle(boardVO);
 
-	    model.addAttribute("result", bdvo);
-	    model.addAttribute("bdMstr", bmvo);	
-		
-	    return "admin/board/view";
-		}
-	
-		if (isAuthenticated) {
-	    final Map<String, MultipartFile> files = multiRequest.getFileMap();
-	    if (!files.isEmpty()) {
-		if ("".equals(atchFileId)) {
-		    List<FileVO> result = fileUtil.parseFileInf(files, "BBS_", 0, atchFileId, "");
-		    atchFileId = fileMngService.insertFileInfs(result);
-		    board.setAtchFileId(atchFileId);
-		} else {
-		    FileVO fvo = new FileVO();
-		    fvo.setAtchFileId(atchFileId);
-		    int cnt = fileMngService.getMaxFileSN(fvo);
-		    List<FileVO> _result = fileUtil.parseFileInf(files, "BBS_", cnt, atchFileId, "");
-		    fileMngService.updateFileInfs(_result);
-		}
-	    }
+       model.addAttribute("result", bdvo);
+       model.addAttribute("bdMstr", bmvo);
 
-	    board.setLastUpdusrId(user.getUniqId());
+      return "admin/board/view";
+      }
+      
+      
+      if (isAuthenticated) {
+          final Map<String, MultipartFile> files = multiRequest.getFileMap();
+          if (!files.isEmpty()) {
+         if ("".equals(atchFileId)) {
+             List<FileVO> result = fileUtil.parseFileInf(files, "BBS_", 0, atchFileId, "");
+             atchFileId = fileMngService.insertFileInfs(result);
+             board.setAtchFileId(atchFileId);
+         } else {
+             FileVO fvo = new FileVO();
+             fvo.setAtchFileId(atchFileId);
+             int cnt = fileMngService.getMaxFileSN(fvo);
+             List<FileVO> _result = fileUtil.parseFileInf(files, "BBS_", cnt, atchFileId, "");
+             fileMngService.updateFileInfs(_result);
+         }
+          }
+   
+          board.setLastUpdusrId(user.getUniqId());
+   
+          board.setNtcrNm("");   // dummy 오류 수정 (익명이 아닌 경우 validator 처리를 위해 dummy로 지정됨)
+          board.setPassword("");   // dummy 오류 수정 (익명이 아닌 경우 validator 처리를 위해 dummy로 지정됨)
+          board.setNttCn(unscript(board.getNttCn()));   // XSS 방지
+   
+          bbsMngService.updateBoardArticle(board);
+      }
 
-	    board.setNtcrNm("");	// dummy 오류 수정 (익명이 아닌 경우 validator 처리를 위해 dummy로 지정됨)
-	    board.setPassword("");	// dummy 오류 수정 (익명이 아닌 경우 validator 처리를 위해 dummy로 지정됨)
-	    board.setNttCn(unscript(board.getNttCn()));	// XSS 방지
-
-	    bbsMngService.updateBoardArticle(board);
-	}
-	return "forward:/admin/board/selectBoard.do";
+   return "forward:/admin/board/selectBoard.do";
    }
    /*
     * AdminLTE용 게시물에 대한 상세보기.
     */
-    @RequestMapping("/admin/board/viewBoard.do")
-       public String viewBoard(@ModelAttribute("searchVO") BoardVO boardVO, ModelMap model) throws Exception {
-          LoginVO user = new LoginVO();
-          if(EgovUserDetailsHelper.isAuthenticated()){
-             user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
-         }
+   @RequestMapping("/admin/board/viewBoard.do")
+    public String viewBoard(@ModelAttribute("searchVO") BoardVO boardVO, ModelMap model) throws Exception {
+       LoginVO user = new LoginVO();
+       if(EgovUserDetailsHelper.isAuthenticated()){
+          user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+      }
 
-         // 조회수 증가 여부 지정
-         boardVO.setPlusCount(true);
+      // 조회수 증가 여부 지정
+      boardVO.setPlusCount(true);
 
-         //---------------------------------
-         // 2009.06.29 : 2단계 기능 추가
-         //---------------------------------
-         if (!boardVO.getSubPageIndex().equals("")) {
-             boardVO.setPlusCount(false);
-         }
-         ////-------------------------------
+      //---------------------------------
+      // 2009.06.29 : 2단계 기능 추가
+      //---------------------------------
+      if (!boardVO.getSubPageIndex().equals("")) {
+          boardVO.setPlusCount(false);
+      }
+      ////-------------------------------
 
-         boardVO.setLastUpdusrId(user.getUniqId());
-         BoardVO vo = bbsMngService.selectBoardArticle(boardVO);
+      boardVO.setLastUpdusrId(user.getUniqId());
+      BoardVO vo = bbsMngService.selectBoardArticle(boardVO);
 
-         model.addAttribute("result", vo);
+      model.addAttribute("result", vo);
 
-         model.addAttribute("sessionUniqId", user.getUniqId());
+      model.addAttribute("sessionUniqId", user.getUniqId());
 
-         //----------------------------
-         // template 처리 (기본 BBS template 지정  포함)
-         //----------------------------
-         BoardMasterVO master = new BoardMasterVO();
+      //----------------------------
+      // template 처리 (기본 BBS template 지정  포함)
+      //----------------------------
+      BoardMasterVO master = new BoardMasterVO();
 
-         master.setBbsId(boardVO.getBbsId());
-         master.setUniqId(user.getUniqId());
+      master.setBbsId(boardVO.getBbsId());
+      master.setUniqId(user.getUniqId());
 
-         BoardMasterVO masterVo = bbsAttrbService.selectBBSMasterInf(master);
+      BoardMasterVO masterVo = bbsAttrbService.selectBBSMasterInf(master);
 
-         if (masterVo.getTmplatCours() == null || masterVo.getTmplatCours().equals("")) {
-             masterVo.setTmplatCours("/css/egovframework/cop/bbs/egovBaseTemplate.css");
-         }
+      if (masterVo.getTmplatCours() == null || masterVo.getTmplatCours().equals("")) {
+          masterVo.setTmplatCours("/css/egovframework/cop/bbs/egovBaseTemplate.css");
+      }
 
-         model.addAttribute("bdMstr", masterVo);
+      model.addAttribute("bdMstr", masterVo);
 
-         return "admin/board/view";
-       }
+      return "admin/board/view";
+    }
    
    /*
     * AdminLTE용 게시물에 대한 목록을 조회한다.
